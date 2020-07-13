7873c3b186b155b36e65eb8dbe080b8662416a47
Waqas Noor <waqas2c@yahoo.com>
2020-07-13 18:35:11 +0500

    Adding Set state to class based counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+  updateCounter = (counter) => {
+    this.setState((state) => {
+      return { ...state, counter };
+    });
+  };
+
   render() {
+    const { counter } = this.state;
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{counter}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={() => this.updateCounter(counter + 1)}>
+            Increment
+          </button>
+          <button onClick={() => this.updateCounter(counter - 1)}>
+            Decrement
+          </button>
+          <button onClick={() => this.updateCounter(0)}>Reset</button>
         </section>
       </div>
     );
